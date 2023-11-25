0679661e6611c94246c604aa2c0dc1839b5df28e
jamesthecooke <jamesjcooke03@gmail.com>
2023-11-25 17:50:36 +0000

    Made custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,34 +1,8 @@
-import React, { useEffect, useState } from 'react'
 import { Text } from '@chakra-ui/react'
-import apiClient from '../services/api-client';
-
-// importing the necessary info for the fetch
-
-//defining the interface that represents the shape of the game data from the api
-
-interface Game {
-    id: number;
-    name: string;
-}
-
-// defining the interface that represents the shape of the object from the api
-
-interface FetchGamesResponse {
-    count: number;
-    results: Game[]
-}
-
+import useGames from '../hooks/useGames'
+//  this code is responsible for returning the mark up - it gets the required information from - useGames
 export const GameGrid = () => {
-    const [games,setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
-// use effect to fetch the data from api
-    useEffect(()=> {
-        apiClient.get<FetchGamesResponse>('/games')
-        // using axios to get the data properites to access the body
-          .then(res => setGames(res.data.results))
-          .catch(err => setError(err.message));
-    })
-
+    const {games, error} = useGames();
   return (
     <>
     {/* will display a error only if a error occurs */}
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+const useGames = () => {
+  //defining the interface that represents the shape of the game data from the api
+
+  interface Game {
+    id: number;
+    name: string;
+  }
+
+  // defining the interface that represents the shape of the object from the api
+
+  interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+  }
+
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  // use effect to fetch the data from api
+
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal})
+      // using axios to get the data properites to access the body
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        // only displays the cancelled message if cancelled
+        if (err instanceof CanceledError) return;
+        setError(err.message)});
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
