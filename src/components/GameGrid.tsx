32d5b6f62c1c9f9d825865674161d4ec1bd40e47
jamesthecooke <jamesjcooke03@gmail.com>
2024-01-15 16:53:50 +0000

    display genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,8 +16,8 @@ export const GameGrid = () => {
       {/* tells us how many columns should be shown for the device size */}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
-        padding={10}
+        spacing={3}
+        padding="10px"
       >
         {isLoading &&
           skeletons.map((skeleton) => (
