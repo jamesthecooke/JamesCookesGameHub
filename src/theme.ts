1a6673b1113dd1fdb1d0055cf699e7713577cc44
jamesthecooke <jamesjcooke03@gmail.com>
2024-01-16 20:59:18 +0000

    customised theme to get darker grays

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,5 @@
 import { ThemeConfig, extendTheme } from "@chakra-ui/react";
+import { color } from "framer-motion";
 
 //  using chakra to creat a dark mode theme 
 // v* using this to access the propertys and set them to dark
@@ -6,6 +7,22 @@ const config: ThemeConfig = {
     initialColorMode: 'dark'
 };
 // storing this data as theme
-const theme = extendTheme({ config });
+const theme = extendTheme({ 
+    config,
+    colors: {
+      gray: {
+        50: '#f9f9f9',
+        100: '#ededed',
+        200: '#d3d3d3',
+        300: '#b3b3b3',
+        400: '#a0a0a0',
+        500: '#898989',
+        600: '#6c6c6c',
+        700: '#202020',
+        800: '#121212',
+        900: '#111',
+      }
+    } 
+});
 
 export default theme;
