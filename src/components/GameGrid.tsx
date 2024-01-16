e5835c9c376a7581d2fe305a5e83688741763dc6
jamesthecooke <jamesjcooke03@gmail.com>
2024-01-16 21:01:15 +0000

    refactor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,12 +16,9 @@ export const GameGrid = ({gameQuery}:Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {/* will display a error only if a error occurs */}
-      {error && <Text>{error}</Text>}
-      {/* simple grid from chakra */}
-      {/* tells us how many columns should be shown for the device size */}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         spacing={6}
@@ -39,6 +36,5 @@ export const GameGrid = ({gameQuery}:Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
   );
 };
